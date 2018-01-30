ac349455cfa30e2fdbceec61981ecfe876af601e
SofusA <sofus@addington.dk>
2018-01-30 16:53:12 +0100

    ting

--- battle-expert.py
+++ battle-expert.py
@@ -1,6 +1,7 @@
 import csv
-with open('pokemon.csv', 'rb') as f:
-    reader = csv.reader(f)
+
+with open('pokemon.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
     pokemon_list = list(reader)
 
 print pokemon_list
