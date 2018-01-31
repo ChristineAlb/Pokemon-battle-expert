d1864fd7ac616deff6086f528531ec8cabf60045
SofusA <sofus@addington.dk>
2018-01-31 12:13:32 +0100

    UPdated scope

--- battle-env/battle-expert/battle-expert.py
+++ battle-env/battle-expert/battle-expert.py
@@ -1,3 +1,6 @@
+"""
+Gotta catch em all!
+"""
 import numpy
 import pandas
 import os
@@ -6,5 +9,16 @@ import os
 dir = os.path.dirname(__file__)
 pokemon_list = os.path.join(dir, 'datasheet/pokemon.csv')
 pokemon_list = pandas.read_csv(pokemon_list)
+battle_list = os.path.join(dir, 'datasheet/combats.csv')
+battle_list = pandas.read_csv(battle_list)
 
-print pokemon_list
+# scope the sheet, by removing all other generations than 1
+# pokemon_list = pokemon_list.drop('#', axis=1)
+pokemon_list = pokemon_list[pokemon_list.Generation == 1]
+pokemon_list = pokemon_list[pokemon_list.Name.str.contains('Mega') == False]
+
+battle_list = battle_list + 1
+battle_list = battle_list[battle_list.First_pokemon <= 166]
+battle_list = battle_list[battle_list.Second_pokemon <= 166]
+
+print(pokemon_list)
