535d5a4793c16a8d21a962c7530bac230d87fab2
SofusA <sofus@addington.dk>
2018-02-05 10:30:41 +0100

    Final scoping
    All non generation 1 pokemons is removed from pokemon_list & battle_list

--- battle-env/battle-expert/battle-expert.py
+++ battle-env/battle-expert/battle-expert.py
@@ -1,24 +1,37 @@
-"""
-Gotta catch em all!
+""".
+
+Gotta catch em all
+
 """
 import numpy
 import pandas
 import os
 
-# Import pokemon data
+# ------ Import pokemon data ------
 dir = os.path.dirname(__file__)
 pokemon_list = os.path.join(dir, 'datasheet/pokemon.csv')
-pokemon_list = pandas.read_csv(pokemon_list)
+pokemonCompleteList = pandas.read_csv(pokemon_list)
 battle_list = os.path.join(dir, 'datasheet/combats.csv')
 battle_list = pandas.read_csv(battle_list)
 
-# scope the sheet, by removing all other generations than 1
-# pokemon_list = pokemon_list.drop('#', axis=1)
-pokemon_list = pokemon_list[pokemon_list.Generation == 1]
+# ------ scope the sheet, by removing all other generations than 1 ------
+pokemon_list = pokemonCompleteList[pokemonCompleteList.Generation == 1]
 pokemon_list = pokemon_list[pokemon_list.Name.str.contains('Mega') == False]
 
-battle_list = battle_list + 1
-battle_list = battle_list[battle_list.First_pokemon <= 166]
-battle_list = battle_list[battle_list.Second_pokemon <= 166]
+# ------ Remove # row ------
+# pokemon_list = pokemon_list.drop('#', axis=1)
+# battle_list = battle_list + 1
 
-print(pokemon_list)
+# ------ Identify deleted pokemon id -----
+irrelevantPokemon = pokemonCompleteList.index[
+    ~pokemonCompleteList.index.isin(pokemon_list.index)] + 1
+
+# ------ Remove unreleveant battles from combats ------
+for i in range(0, len(irrelevantPokemon)):
+    battle_list = battle_list[
+        battle_list.First_pokemon != irrelevantPokemon[i]]
+    battle_list = battle_list[
+        battle_list.Second_pokemon != irrelevantPokemon[i]]
+    battle_list = battle_list[battle_list.Winner != irrelevantPokemon[i]]
+
+print(battle_list)
