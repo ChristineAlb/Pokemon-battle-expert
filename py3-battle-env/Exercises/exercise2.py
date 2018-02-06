7562c32e54529d4a57fff3b9326cb73671923fe4
SofusA <sofus@addington.dk>
2018-02-06 15:50:33 +0100

    ting ting

--- py3-battle-env/Exercises/exercise2.py
+++ py3-battle-env/Exercises/exercise2.py
@@ -1,7 +1,10 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import os
 import xlrd
+
+from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show
+from scipy.linalg import svd
 
 # ------ Import data ----- #
 dir = os.path.dirname(__file__)
@@ -17,17 +20,59 @@ classLabels = doc.col_values(0, 2, 92)
 classNames = sorted(set(classLabels))
 classDict = dict(zip(classNames, range(5)))
 
-# Extract vector y, convert to NumPy matrix and transpose
-y = numpy.mat([classDict[value] for value in classLabels]).T
+# Extract vector y, convert to np matrix and transpose
+y = np.mat([classDict[value] for value in classLabels]).T
 
 # Preallocate memory, then extract excel data to matrix X
-X = numpy.mat(numpy.empty((90, 8)))
+X = np.mat(np.empty((90, 8)))
 for i, col_id in enumerate(range(3, 11)):
-    X[:, i] = numpy.mat(doc.col_values(col_id, 2, 92)).T
+    X[:, i] = np.mat(doc.col_values(col_id, 2, 92)).T
 
 # Compute values of N, M and C.
 N = len(y)
 M = len(attributeNames)
 C = len(classNames)
 
-print(y)
+# Data attributes to be plotted
+i = 0
+j = 1
+
+##
+# Make a simple plot of the i'th attribute against the j'th attribute
+# Notice that X is of matrix type (but it will also work with a np array)
+# X = np.array(X) #Try to uncomment this line
+# plot(X[:, i], X[:, j], 'o')
+
+# %%
+# Make another more fancy plot that includes legend, class labels,
+# attribute names, and a title.
+f = figure()
+title('NanoNose data')
+
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y.A.ravel() == c
+    plot(X[class_mask, i], X[class_mask, j], 'o')
+
+legend(classNames)
+xlabel(attributeNames[i])
+ylabel(attributeNames[j])
+
+# Output result to screen
+# show()
+# Subtract mean value from data
+Y = X - np.ones((N, 1))*X.mean(0)
+
+# PCA by computing SVD of Y
+U, S, V = svd(Y, full_matrices=False)
+
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum()
+
+# Plot variance explained
+figure()
+plot(range(1, len(rho)+1), rho, 'o-')
+title('Variance explained by principal components')
+xlabel('Principal component')
+ylabel('Variance explained')
+show()
